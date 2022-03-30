ecca2bd6d65c49f07c81963e751218efacc4797c
aislingmurray <aislingmurray700@gmail.com>
2022-03-30 19:34:14 +0100

    Basics
    1 hour in.

---
+++ MyFirstApp/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";// This will output to the console "Hello World!"#
+    cout << "Todays is Wednesday." << endl;
+    cout << "My name is Aisling." << endl;
+    cout << "Have a nice day." << endl;
+
+    system("pause>0");
+}
+//cout = console output

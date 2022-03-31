108381a273a8171bf8431acd508464e2cd180013
aislingmurray <aislingmurray700@gmail.com>
2022-03-31 22:40:13 +0100

    Datatype Overflow
    Example of the int max + 1.

---
+++ Application2a/Application2a/Application2a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int intMax = INT_MAX;
+	//The max int available
+	cout << intMax << endl;
+	//Datatype Overflow! Max int plus 1 goes to the min int 
+	cout << intMax + 1;
+
+	system("pause>0");
+}

1fe1e2e9982967eb63c96fec7662c427507542d5
aislingmurray <aislingmurray700@gmail.com>
2022-04-02 23:30:18 +0100

    IfElse
    Testing to see if a number is even or odd using an if else statement.

---
+++ IfElse/IfElse/IfElse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//User enters integer number
+	//Program writes out if the number is even or odd
+	
+	int number;
+	cout << "Enter whole number: ";
+	cin >> number;
+
+	if (number % 2 == 0)
+	{
+		cout << number << " is an even number." << endl;
+	}
+	else
+	{
+		cout << number << " is an odd number." << endl;
+	}
+
+	system("pause>0");
+}
